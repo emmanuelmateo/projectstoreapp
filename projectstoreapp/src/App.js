3ad8dcbcb452ffe3e84ec9fb52792d046e7acfc6
eysenck <eysenck.mateo@utec.edu.pe>
2021-02-20 17:45:03 -0500

    commit form

--- projectstoreapp/src/App.js
+++ projectstoreapp/src/App.js
@@ -1,17 +1,28 @@
 import React from 'react'
 // import HomeView from './views/HomeView'
 // import DetalleView from './views/DetalleView'
-import Formulario from './components/Formulario'
+import Narbar from './components/Navbar';
+import Formulario from './components/Formulario';
 
 import {Switch, BrowserRouter as Router} from "react-router-dom"
-import Routes from "./routes"
+import Routes from "./routes";
+import Navbar from './components/Navbar';
+import Carousel from './components/Carousel';
 
 export default function App() {
   return (
     <Router>
+     
       <div className="container">
+        <Navbar/>
+        
+        {/* <h1>Navbar</h1> */}
         <Switch>
-          <Routes />
+          <Routes path="/" exact/>
+            
+          <Routes path ="/Formulario">
+            <Formulario/>
+          </Routes>
         </Switch>
       </div>
     </Router>
--- projectstoreapp/src/components/Formulario.js
+++ projectstoreapp/src/components/Formulario.js
@@ -1,26 +1,57 @@
 import React, {useState, Fragment} from 'react';
 
 const Formulario = ()=>{
+
+        const [datos, setDatos] = useState({
+            nombre: '',
+            mail:'',
+            password:'',
+            password:'',
+
+        })
     return(
         <Fragment>
             <h1>Crear cuenta</h1>    
             <form ClassName="row"> 
                 <div className="col-md-3">
-                    <input></input>
+                    <input
+                    placeholder = "Ingrese Nombre"
+                    ClassName="form-control"
+                    type="text"
+                    name="nombre"
+                    onChange={handleInputChange}//el encargado de estar pendiente de mi input
+                    ></input>
                 </div>
                 <div className="col-md-3">
-                    <input></input>
+                    <input
+                    placeholder = "Ingrese Nombre"
+                    ClassName="form-control"
+                    type="email"
+                    name="mail">
+                    </input>
                 </div>
                 <div className="col-md-3">
-                    <input></input>
+                    <input
+                    placeholder = "Ingrese Nombre"
+                    ClassName="form-control"
+                    type="password"
+                    name="password"
+                    ></input>
                 </div>
                 <div className="col-md-3">
-                    <input></input>
+                    <input
+                     placeholder = "Ingrese Nombre"
+                     ClassName="form-control"
+                     type="password"
+                     name="password1"
+                     ></input>
                 </div>
                 <div className="col-md-3">
-                    <button>Crear cuenta</button>
+                    <button ClassName="btn btn-primary" type="submit">Crear cuenta</button>
                 </div>
             </form>
         </Fragment>
     )
 }
+
+export default Formulario;
